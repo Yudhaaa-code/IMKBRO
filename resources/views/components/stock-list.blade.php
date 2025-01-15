2cf08f1b4c382f85c6bff56cceaa8063465d6381
akuabaw <akbarsaputra935@gmail.com>
2025-01-16 06:33:49 +0700

    pembaruan produk list

---
+++ resources/views/components/stock-list.blade.php
@@ -0,0 +1,142 @@
+<div class="font-[sans-serif] p-4 mx-auto lg:max-w-5xl max-w-2xl">
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+
+      <div
+        class="bg-white border overflow-hidden rounded-2xl cursor-pointer hover:border-blue-600 transition-all relative">
+        <div class="bg-gray-50 overflow-hidden mx-auto rounded-b-2xl">
+          <img src="https://readymadeui.com/images/sunglass1.webp" alt="sunglass1"
+            class="aspect-[281/218] w-full object-contain" />
+        </div>
+
+        <div class="p-4">
+          <h3 class="text-sm sm:text-base font-bold text-gray-800">Golden Glass Wear</h3>
+          <div class="flex items-center justify-between gap-2 mt-4">
+            <div
+              class="w-8 h-8 sm:w-10 sm:h-10  bg-gray-100 flex items-center justify-center rounded-full">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-gray-800 inline-block" viewBox="0 0 64 64">
+                <path
+                  d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                  data-original="#000000"></path>
+              </svg>
+            </div>
+            <h4 class="text-sm sm:text-base text-gray-800 font-bold">$10.00</h4>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="bg-white border overflow-hidden rounded-2xl cursor-pointer hover:border-blue-600 transition-all relative">
+        <div class="bg-gray-50 overflow-hidden mx-auto rounded-b-2xl">
+          <img src="https://readymadeui.com/images/sunglass2.webp" alt="sunglass2"
+            class="aspect-[281/218] w-full object-contain" />
+        </div>
+
+        <div class="p-4">
+          <h3 class="text-sm sm:text-base font-bold text-gray-800">Blue Glass Wear</h3>
+          <div class="flex items-center justify-between gap-2 mt-4">
+            <div
+              class="w-8 h-8 sm:w-10 sm:h-10  bg-gray-100 flex items-center justify-center rounded-full">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-gray-800 inline-block" viewBox="0 0 64 64">
+                <path
+                  d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                  data-original="#000000"></path>
+              </svg>
+            </div>
+            <h4 class="text-sm sm:text-base text-gray-800 font-bold">$9.00</h4>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="bg-white border overflow-hidden rounded-2xl cursor-pointer hover:border-blue-600 transition-all relative">
+        <div class="bg-gray-50 overflow-hidden mx-auto rounded-b-2xl">
+          <img src="https://readymadeui.com/images/sunglass5.webp" alt="sunglass3"
+            class="aspect-[281/218] w-full object-contain" />
+        </div>
+
+        <div class="p-4">
+          <h3 class="text-sm sm:text-base font-bold text-gray-800">Black Glass Wear</h3>
+          <div class="flex items-center justify-between gap-2 mt-4">
+            <div
+              class="w-8 h-8 sm:w-10 sm:h-10  bg-gray-100 flex items-center justify-center rounded-full">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-gray-800 inline-block" viewBox="0 0 64 64">
+                <path
+                  d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                  data-original="#000000"></path>
+              </svg>
+            </div>
+            <h4 class="text-sm sm:text-base text-gray-800 font-bold">$16.00</h4>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="bg-white border overflow-hidden rounded-2xl cursor-pointer hover:border-blue-600 transition-all relative">
+        <div class="bg-gray-50 overflow-hidden mx-auto rounded-b-2xl">
+          <img src="https://readymadeui.com/images/sunglass3.webp" alt="sunglass4"
+            class="aspect-[281/218] w-full object-contain" />
+        </div>
+
+        <div class="p-4">
+          <h3 class="text-sm sm:text-base font-bold text-gray-800">Round Glass</h3>
+          <div class="flex items-center justify-between gap-2 mt-4">
+            <div
+              class="w-8 h-8 sm:w-10 sm:h-10  bg-gray-100 flex items-center justify-center rounded-full">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-gray-800 inline-block" viewBox="0 0 64 64">
+                <path
+                  d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                  data-original="#000000"></path>
+              </svg>
+            </div>
+            <h4 class="text-sm sm:text-base text-gray-800 font-bold">$14.00</h4>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="bg-white border overflow-hidden rounded-2xl cursor-pointer hover:border-blue-600 transition-all relative">
+        <div class="bg-gray-50 overflow-hidden mx-auto rounded-b-2xl">
+          <img src="https://readymadeui.com/images/sunglass7.webp" alt="sunglass5"
+            class="aspect-[281/218] w-full object-contain" />
+        </div>
+
+        <div class="p-4">
+          <h3 class="text-sm sm:text-base font-bold text-gray-800">White Lens Glass</h3>
+          <div class="flex items-center justify-between gap-2 mt-4">
+            <div
+              class="w-8 h-8 sm:w-10 sm:h-10  bg-gray-100 flex items-center justify-center rounded-full">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-gray-800 inline-block" viewBox="0 0 64 64">
+                <path
+                  d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                  data-original="#000000"></path>
+              </svg>
+            </div>
+            <h4 class="text-sm sm:text-base text-gray-800 font-bold">$8.00</h4>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="bg-white border overflow-hidden rounded-2xl cursor-pointer hover:border-blue-600 transition-all relative">
+        <div class="bg-gray-50 overflow-hidden mx-auto rounded-b-2xl">
+          <img src="https://readymadeui.com/images/sunglass6.webp" alt="sunglass6"
+            class="aspect-[281/218] w-full object-contain" />
+        </div>
+
+        <div class="p-4">
+          <h3 class="text-sm sm:text-base font-bold text-gray-800">Black Glass</h3>
+          <div class="flex items-center justify-between gap-2 mt-4">
+            <div
+              class="w-8 h-8 sm:w-10 sm:h-10  bg-gray-100 flex items-center justify-center rounded-full">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-gray-800 inline-block" viewBox="0 0 64 64">
+                <path
+                  d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
+                  data-original="#000000"></path>
+              </svg>
+            </div>
+            <h4 class="text-sm sm:text-base text-gray-800 font-bold">$11.00</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
