04a0b8dbe93440e535730b97b91e793c68cc814f
akuabaw <akbarsaputra935@gmail.com>
2025-01-16 03:59:47 +0700

    terbaru tampilan home

---
+++ resources/views/components/product-list.blade.php
@@ -0,0 +1,51 @@
+
+<div class="mt-10">
+  <h2 class="font-medium md:text-lg">🔥 Populer Sekarang</h2>
+  <div class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4">
+    <a class="block relative rounded-lg lg:rounded-xl hover:scale-105 overflow-hidden transition-all duration-300 ease-out group" href="mlbb">
+      <img alt="" loading="lazy" width="384" height="512" decoding="async" data-nimg="1" class="w-full h-full object-cover" style="color:transparent" srcset="img/fifa.jpg">
+      <div class="hidden lg:block absolute inset-0 dark:border-2 border-transparent group-hover:border-zinc-50 rounded-lg lg:rounded-xl group-hover:bg-gradient-to-t from-zinc-950/90 via-zinc-950/50 to-transparent transition-all duration-300 ease-out"></div><div class="hidden lg:block absolute left-5 right-5 -bottom-20 group-hover:bottom-5 text-zinc-100 transition-all duration-300 ease-out">
+        <h3 class="font-medium truncate">FIFA Mobile</h3>
+        <p class="text-xs truncate">EA Sport</p>
+      </div>
+    </a>
+    <a class="block relative rounded-lg lg:rounded-xl hover:scale-105 overflow-hidden transition-all duration-300 ease-out group" href="canva">
+      <img alt="" loading="lazy" width="384" height="512" decoding="async" data-nimg="1" class="w-full h-full object-cover" style="color:transparent" srcset="/_next/image?url=%2Fassets%2Fhiburan%2Fcanva%2Fposter.png&w=384&q=75 1x, /_next/image?url=%2Fassets%2Fhiburan%2Fcanva%2Fposter.png&w=828&q=75 2x" src="/_next/image?url=%2Fassets%2Fhiburan%2Fcanva%2Fposter.png&w=828&q=75">
+      <div class="hidden lg:block absolute inset-0 dark:border-2 border-transparent group-hover:border-zinc-50 rounded-lg lg:rounded-xl group-hover:bg-gradient-to-t from-zinc-950/90 via-zinc-950/50 to-transparent transition-all duration-300 ease-out"></div>
+      <div class="hidden lg:block absolute left-5 right-5 -bottom-20 group-hover:bottom-5 text-zinc-100 transition-all duration-300 ease-out">
+        <h3 class="font-medium truncate">Canva</h3>
+        <p class="text-xs truncate">Canva</p>
+      </div>
+    </a>
+    <a class="block relative rounded-lg lg:rounded-xl hover:scale-105 overflow-hidden transition-all duration-300 ease-out group" href="free-fire"><img alt="" loading="lazy" width="384" height="512" decoding="async" data-nimg="1" class="w-full h-full object-cover" style="color:transparent" srcset="/_next/image?url=%2Fassets%2Fgames%2Fmobile%2Ffree-fire%2Fposter.png&w=384&q=75 1x, /_next/image?url=%2Fassets%2Fgames%2Fmobile%2Ffree-fire%2Fposter.png&w=828&q=75 2x" src="/_next/image?url=%2Fassets%2Fgames%2Fmobile%2Ffree-fire%2Fposter.png&w=828&q=75">
+      <div class="hidden lg:block absolute inset-0 dark:border-2 border-transparent group-hover:border-zinc-50 rounded-lg lg:rounded-xl group-hover:bg-gradient-to-t from-zinc-950/90 via-zinc-950/50 to-transparent transition-all duration-300 ease-out"></div>
+      <div class="hidden lg:block absolute left-5 right-5 -bottom-20 group-hover:bottom-5 text-zinc-100 transition-all duration-300 ease-out">
+        <h3 class="font-medium truncate">Free Fire</h3>
+        <p class="text-xs truncate">Garena</p>
+      </div>
+    </a>
+    <a class="block relative rounded-lg lg:rounded-xl hover:scale-105 overflow-hidden transition-all duration-300 ease-out group" href="roblox">
+      <img alt="" loading="lazy" width="384" height="512" decoding="async" data-nimg="1" class="w-full h-full object-cover" style="color:transparent" srcset="/_next/image?url=%2Fassets%2Fvouchers%2Froblox%2Fposter.png&;w=384&q=75 1x, /_next/image?url=%2Fassets%2Fvouchers%2Froblox%2Fposter.png&w=828&q=75 2x" src="/_next/image?url=%2Fassets%2Fvouchers%2Froblox%2Fposter.png&w=828&q=75">
+      <div class="hidden lg:block absolute inset-0 dark:border-2 border-transparent group-hover:border-zinc-50 rounded-lg lg:rounded-xl group-hover:bg-gradient-to-t from-zinc-950/90 via-zinc-950/50 to-transparent transition-all duration-300 ease-out"></div>
+      <div class="hidden lg:block absolute left-5 right-5 -bottom-20 group-hover:bottom-5 text-zinc-100 transition-all duration-300 ease-out">
+        <h3 class="font-medium truncate">Roblox</h3>
+        <p class="text-xs truncate">Roblox Corporation</p>
+      </div>
+    </a>
+    <a class="block relative rounded-lg lg:rounded-xl hover:scale-105 overflow-hidden transition-all duration-300 ease-out group" href="by-u">
+      <img alt="" loading="lazy" width="384" height="512" decoding="async" data-nimg="1" class="w-full h-full object-cover" style="color:transparent" srcset="/_next/image?url=%2Fassets%2Fpulsa-dan-paket%2Fby-u%2Fposter.png&w=384&q=75 1x, /_next/image?url=%2Fassets%2Fpulsa-dan-paket%2Fby-u%2Fposter.png&w=828&q=75 2x" src="/_next/image?url=%2Fassets%2Fpulsa-dan-paket%2Fby-u%2Fposter.png&w=828&q=75">
+      <div class="hidden lg:block absolute inset-0 dark:border-2 border-transparent group-hover:border-zinc-50 rounded-lg lg:rounded-xl group-hover:bg-gradient-to-t from-zinc-950/90 via-zinc-950/50 to-transparent transition-all duration-300 ease-out"></div>
+      <div class="hidden lg:block absolute left-5 right-5 -bottom-20 group-hover:bottom-5 text-zinc-100 transition-all duration-300 ease-out">
+        <h3 class="font-medium truncate">by.U</h3>
+        <p class="text-xs truncate">PT Telekomunikasi Indonesia</p>
+      </div>
+    </a>
+    <a class="block relative rounded-lg lg:rounded-xl hover:scale-105 overflow-hidden transition-all duration-300 ease-out group" href="valorant"><img alt="" loading="lazy" width="384" height="512" decoding="async" data-nimg="1" class="w-full h-full object-cover" style="color:transparent" srcset="/_next/image?url=%2Fassets%2Fgames%2Fpc%2Fvalorant%2Fposter.png&w=384&q=75 1x, /_next/image?url=%2Fassets%2Fgames%2Fpc%2Fvalorant%2Fposter.png&w=828&q=75 2x" src="/_next/image?url=%2Fassets%2Fgames%2Fpc%2Fvalorant%2Fposter.png&w=828&q=75">
+      <div class="hidden lg:block absolute inset-0 dark:border-2 border-transparent group-hover:border-zinc-50 rounded-lg lg:rounded-xl group-hover:bg-gradient-to-t from-zinc-950/90 via-zinc-950/50 to-transparent transition-all duration-300 ease-out"></div>
+      <div class="hidden lg:block absolute left-5 right-5 -bottom-20 group-hover:bottom-5 text-zinc-100 transition-all duration-300 ease-out">
+        <h3 class="font-medium truncate">Valorant</h3>
+        <p class="text-xs truncate">Riot Games</p>
+      </div>
+    </a>
+  </div>
+</div>
