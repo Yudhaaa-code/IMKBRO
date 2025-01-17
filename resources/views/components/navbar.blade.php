58cd9d07b7b35091dde0ed1fb75f98db50484d05
akuabaw <akbarsaputra935@gmail.com>
2025-01-17 22:41:26 +0700

    v.1.2

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,76 +1,81 @@
-<nav class="bg-zinc-900 shadow-md border-b border-gray-600 bg-opacity-90 backdrop-blur-md sticky top-0 z-50">
-    <div class="p-4 mx-auto lg:max-w-5xl max-w-2xl">
-      <div class="flex h-16 items-center justify-between">
-        <div class="flex items-center">
-          <div class="shrink-0">
-            <a class="flex items-center gap-3" href="/">
-            <img class="size-32" src="img/logo-removebg-preview.png"  alt="Your Company">
-            </a>
-          </div>
-          <div class="hidden md:block text-zinc-100">
-            <div class="ml-10 flex items-baseline space-x-4">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a class="relativeflex items-center space-x-2 text-white hover:text-gray-300" href="/">
-                <div class="flex items-center gap-2 text-base">
-                  <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.2" baseProfile="tiny" viewBox="0 0 24 24" class="size-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 3s-6.186 5.34-9.643 8.232c-.203.184-.357.452-.357.768 0 .553.447 1 1 1h2v7c0 .553.447 1 1 1h3c.553 0 1-.448 1-1v-4h4v4c0 .552.447 1 1 1h3c.553 0 1-.447 1-1v-7h2c.553 0 1-.447 1-1 0-.316-.154-.584-.383-.768-3.433-2.892-9.617-8.232-9.617-8.232z">
-                    </path>
-                  </svg>
-                  Beranda
-                </div>
-            </a>
-            </div>
-          </div>
-        </div>
-        <div class="relative w-49 transition-all">
-          <input
-            type="text"
-            placeholder="Search..."
-            id="searchInput"
-            class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-full outline-none transition-all border-gray-300"
-          />
-          <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-          </div>
-        </div>
-          <!-- Mobile menu button -->
-        </div>
-      </div>
-    </div>
-    <div class="relative w-49 transition-all">
-      <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        </svg>
-      </div>
-    </div>
-  
-    <script>
-      const searchInput = document.getElementById('searchInput');
-      const searchContainer = searchInput.parentElement;
-      const searchIcon = searchContainer.querySelector('svg');
-  
-      searchInput.addEventListener('focus', () => {
-        searchContainer.classList.add('w-64');
-        searchContainer.classList.remove('w-48');
-        searchInput.classList.remove('border-gray-300');
-        searchInput.classList.add('border-gray-400');
-        // searchIcon.classList.add('text-blue-500');
-        searchIcon.classList.remove('text-gray-400');
-      });
-  
-      searchInput.addEventListener('blur', () => {
-        searchContainer.classList.add('w-48');
-        searchContainer.classList.remove('w-64', 'bg-white');
-        searchInput.classList.add('border-gray-300');
-        // searchInput.classList.remove('border-blue-500');
-        searchIcon.classList.add('text-gray-400');
-        // searchIcon.classList.remove('text-blue-500');
-      });
-    </script>
-        </div>
-      </div>
-    </div>
 
-  </nav>
+<nav x-data="{ isOpen: false }" class="bg-zinc-900 shadow-md border-b border-gray-600 bg-opacity-90 backdrop-blur-md sticky top-0 z-50">
+  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div class="relative flex h-16 items-center justify-between">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <!-- Mobile menu button-->
+        <button  @click="isOpen = !isOpen" type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400" aria-controls="mobile-menu" aria-expanded="false">
+          <span class="absolute -inset-0.5"></span>
+          <span class="sr-only">Open main menu</span>
+          <!--
+            Icon when menu is closed.
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+          <svg :class="{'hidden': isOpen, 'block': !isOpen }" class="block size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+          <!--
+            Icon when menu is open.
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+          <svg :class="{'block': isOpen, 'hidden': !isOpen }" class="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex shrink-0 items-center">
+          <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+        </div>
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex space-x-4">
+            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+            <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-white  hover:text-zinc-400" aria-current="page">Beranda</a>
+            <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-white hover:text-zinc-400">Catalog</a>
+          </div>
+        </div>
+      </div>
+      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        
+
+        <!-- Profile dropdown -->
+        <div class="relative ml-3">
+
+          <!--
+            Dropdown menu, show/hide based on menu state.
+
+            Entering: "transition ease-out duration-100"
+              From: "transform opacity-0 scale-95"
+              To: "transform opacity-100 scale-100"
+            Leaving: "transition ease-in duration-75"
+              From: "transform opacity-100 scale-100"
+              To: "transform opacity-0 scale-95"
+          -->
+          <div x-show="isOpen"
+          x-transition:enter="transition ease-out duration-100 transform"
+          x-transition:enter-start="opacity-0 scale-95"
+          x-transition:enter-end="opacity-100 scale-100"
+          x-transition:leave="transition ease-in duration-75 transform"
+          x-transition:leave-start="opacity-100 scale-100"
+          x-transition:leave-end="opacity-0 scale-95"
+          class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg"
+          class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+            <!-- Active: "bg-gray-100 outline-none", Not Active: "" -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div x-show="isOpen" class="sm:hidden" id="mobile-menu">
+    <div class="space-y-1 px-2 pb-3 pt-2">
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+      <a href="/" class="block rounded-md px-3 py-2 text-base font-medium text-white hover:text-zinc-400" aria-current="page">Beranda</a>
+      <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-white hover:text-zinc-400">Catalog</a>
+    </div>
+  </div>
+
+</nav>
--- resources/views/components/stock-list.blade.php
+++ resources/views/components/stock-list.blade.php
@@ -1,4 +1,4 @@
-<div class="font-[sans-serif] bbg-zinc-900 mt-10 p-4 mx-auto lg:max-w-5xl max-w-2xl">
+<div class="font-[sans-serif] mt-10 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="p-4 mx-auto lg:max-w-7xl md:max-w-4xl sm:max-w-xl max-sm:max-w-sm">
         <h2 class="text-2xl sm:text-3xl font-extrabold text-zinc-100 mb-1 sm:mb-4">Mobile Legend: Bang Bang</h2>
           <p class="text-2xl sm:text-2xl text-zinc-100 mb-1 sm:mb-4">Montoon</p>
@@ -16,16 +16,7 @@
               </div>
               <p class="text-zinc-100 text-[13px] mt-2 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               <div class="flex items-center gap-2 mt-4 ">
-                <div
-                  class="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer" title="Wishlist">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16px" class="fill-pink-600
-                   inline-block" viewBox="0 0 64 64">
-                    <path
-                      d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                      data-original="#000000"></path>
-                  </svg>
-                </div>
-                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Add to cart</button>
+                <button type="button" class="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded">Buy Now</button>
               </div>
             </div>
           </div>
