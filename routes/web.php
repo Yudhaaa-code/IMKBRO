50f055b726cd8f64f971173394796605a0868b5f
akuabaw <akbarsaputra935@gmail.com>
2025-01-19 00:15:19 +0700

    v 1.2.1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\imageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,9 @@ Route::get('/mlbb', function () {
     return view('mlbb');
 });
 
+Route::get('/product-detail', function () {
+    return view('/product-detail');
+});
+ 
+Route::resource('/home', imageController::class);
+Route::get('/', [imageController::class, 'index']);
